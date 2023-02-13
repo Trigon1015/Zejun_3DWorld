f4a00b06fbd61238d3bbbe1f76d57ca7c7b6a04f
Trigon1015 <zz2464@nyu.edu>
2023-02-13 15:56:05 -0500

    L3

---
+++ Unity3DStudio/Assets/Scripts/Manager/LightManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb3e0905310eb08d65892dffe3087c1201793602c485b02888c3e0bfec4b713a
+size 1217
--- Unity3DStudio/Assets/Scripts/Planet/GravityOrbit.cs
+++ Unity3DStudio/Assets/Scripts/Planet/GravityOrbit.cs
@@ -1,16 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GravityOrbit : MonoBehaviour
-{
-    public float Gravity;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.GetComponent<GravityControl>())
-        {
-            other.GetComponent<GravityControl>().Gravity = this.GetComponent<GravityOrbit>();
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1a111399473e9e93d684d7a43aaa1b2e20e537df229c0dda18d36cb75db5804
+size 376
