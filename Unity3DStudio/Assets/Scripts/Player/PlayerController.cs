7f2409f5da1ace5bd2c98d6854105ca4249ba09b
Trigon1015 <zz2464@nyu.edu>
2023-03-05 23:56:03 -0500

    sun texture add

--- Unity3DStudio/Assets/Scripts/Player/PlayerController.cs
+++ Unity3DStudio/Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a679f4b26714db48be67512e9bc11846190344fac66e1d695a40e85e35705ab
-size 2834
+oid sha256:2ddd24dc1ff74bf46482fb9b34c2c736c989313b0b298221cbb6ae260564eabe
+size 2836
---
+++ Unity3DStudio/Assets/Solar_System_Assets/Shaders/Atmosphere.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:05c087921e9df0e08831082490c7c05a61b953df2b5a42cf7987569f95bb656d
+size 5983
